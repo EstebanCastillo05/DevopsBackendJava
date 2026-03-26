17b72665f012aaf74a33311331b25e92f9ce28c3
Esteban Jose Adrian Castillo Molina <202300862@estudiantes.udv.gt>
2026-03-25 20:11:57 -0600

    Prueba registry

--- dockerfile
+++ dockerfile
@@ -1,16 +1,8 @@
-FROM maven:3.8.5-openjdk-17 AS build
-
-COPY . /app
-
-WORKDIR /app
-
-RUN mvn clean package -DskipTests
-
 FROM openjdk:17-jdk-slim
 
 WORKDIR /app
 
-COPY --from=build /app/target/*.jar app.jar
+COPY target/*.jar app.jar
 
 EXPOSE 8080
 
--- src/main/java/com/backdevops/backend/services/SaludoService.java
+++ src/main/java/com/backdevops/backend/services/SaludoService.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class SaludoService {
 
     public String obtenerSaludo() {
-        return "Hola desde SpringBoot";
+        return "Hola desde SpringBoot guardado de jar y envio a registry";
     }
 }
